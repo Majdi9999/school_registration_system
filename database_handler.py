c741b9dbc9b4c1ba39ace6636cd24c85ad92c67b
MajdiYasin <majdiyasin523@gmail.com>
2025-06-16 12:12:47 +0300

    My initial commit

---
+++ database_handler.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+class DataBaseHandler:
+    DB_NAME = 'students.db'
+    
+    @staticmethod
+    def _connection():
+        return sqlite3.connect(DataBaseHandler.DB_NAME)
+    
+    @staticmethod
+    def create_table():
+        with DataBaseHandler._connect() as conn:
+            conn.execute("""
+            CREATE TABLE IF NOT EXISTS students (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                age INTEGER NOT NULL,
+                gender TEXT NOT NULL
+            )
+            """)
+
+    @staticmethod
+    def insert_student(name,email,age,gender):
+        with DataBaseHandler._connect() as conn:
+            conn.execute('INSERT INTO students(name,email,age,gender) VALUES (?,?,?,?)',(name,email,age,gender))
+
+    @staticmethod
+    def read_all_students():
+        with DataBaseHandler._connect() as conn:
+            return conn.execute('SELECT * FROM students').fetchall()
+
+DataBaseHandler.create_table()
---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from Registration_Form import RegistrationForm
+
+
+class MainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Student Managment System")
+        self.geometry("900x600")
+        self.create_widgets()
+
+    def create_widgets(self):
+        title_lable = tk.Label(self, text="Students Management System", font=('Helvetica',16))
+        title_lable.pack(side='top',fill='x')
+        self.registeration_form = RegistrationForm(self)
+        
+
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.mainloop()
